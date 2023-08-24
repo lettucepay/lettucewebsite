87617b58f64c51941765ebe49c8f0f0a63b49786
lettucepay <lettucepaygh.com>
2023-08-24 12:23:19 -0400

    added graphics

--- sustainability.php
+++ sustainability.php
@@ -50,14 +50,14 @@
           <div class="col-md-7">
             <h2 class="featurette-heading">Susainable Development Goals<span class="text-muted"></span></h2>
             <p class="lead exp">We firmly believe that our actions today shape tomorrow's world. Our commitment to the United Nations Sustainable Development Goals underpins everything we do:</p>
+            <br>
+            <p class="exp">At Lettuce Pay, we're not just a "buy now pay later" company – we're agents of change. Our mission is to revolutionize the way payments are made, making life easier and more accessible for individuals and businesses across Ghana. Through strategic partnerships with hospitals, e-commerce ventures, and essential service providers, we're fostering a new era of convenience and inclusivity.</p>
           </div>
           <div class="col-md-5">
-            <img class="featurette-image img-fluid mx-auto" src="images/sdg.png" alt="Generic placeholder image">
+            <a href="https://sdgs.un.org/goals"><img class="featurette-image img-fluid mx-auto" src="images/sustain2.png" alt="Generic placeholder image"></a>
           </div>
 
-          <div class="content">
-            At Lettuce Pay, we're not just a "buy now pay later" company – we're agents of change. Our mission is to revolutionize the way payments are made, making life easier and more accessible for individuals and businesses across Ghana. Through strategic partnerships with hospitals, e-commerce ventures, and essential service providers, we're fostering a new era of convenience and inclusivity.
-          </div>
+       
         </div>
 
         <hr class="featurette-divider">
@@ -72,6 +72,50 @@
           <div class="col-md-5 order-md-1">
             <a href="https://sdgs.un.org/goals/goal8"><img class="featurette-image img-fluid mx-auto" src="images/economic.jpg" alt="Generic placeholder image"></a>
           </div>
+        </div>
+        <hr class="featurette-divider">
+
+          <div class="row featurette">
+          <div class="col-md-7">
+            <h2 class="featurette-heading">Industry, Innovation, and Infrastructure<span class="text-muted"></span></h2>
+           
+            <p class="exp">Innovation is in our DNA. We're at the forefront of pioneering payment solutions that enhance convenience and bridge gaps in traditional systems. Our partnerships with various sectors, from healthcare to e-commerce, foster resilient infrastructure and technological advancements, propelling Ghana's progress..</p>
+          </div>
+          <div class="col-md-5">
+            <a href="https://sdgs.un.org/goals/goal9"><img class="featurette-image img-fluid mx-auto" src="images/innovation2.png" alt="Generic placeholder image"></a>
+          </div>
+
+       
+        </div>
+
+        
+        <hr class="featurette-divider">
+
+         <div class="row featurette">
+          <div class="col-md-7 order-md-2">
+            <h3 class="featurette-heading">Climate Action<span class="text-muted"></span></h3>
+            <p class="exp">The environment matters to us. We understand the urgency of addressing climate change. Our digital-first approach minimizes paper waste, reducing our carbon footprint. Through efficient operations and collaborations, we're striving for a greener, more sustainable future</p>
+            
+         
+          </div>
+          <div class="col-md-5 order-md-1">
+            <a href="https://sdgs.un.org/goals/goal13"><img class="featurette-image img-fluid mx-auto" src="images/climate.png" alt="Generic placeholder image"></a>
+          </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+          <div class="col-md-7">
+            <h2 class="featurette-heading">Partnerships for the Goals<span class="text-muted"></span></h2>
+         
+            <p class="exp">Collaboration drives change. We stand by this principle. By forging partnerships with hospitals, e-commerce businesses, and essential service providers, we're collectively advancing towards common goals. Lettuce Pay believes that uniting diverse stakeholders is pivotal to achieving lasting positive impact.</p>
+          </div>
+          <div class="col-md-5">
+            <a href="https://sdgs.un.org/goals17"><img class="featurette-image img-fluid mx-auto" src="images/partnership2.png" alt="Generic placeholder image"></a>
+          </div>
+
+       
         </div>
 			
 		</div>
